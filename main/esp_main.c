02f6626f263625095b7ceee2a49a77f58c0c6034
Sumio Kiyooka <sumio@memfault.com>
2025-11-15 16:38:10 -0500

    Mount fat, compile mud source, and enter mud main loop

--- main/esp_main.c
+++ main/esp_main.c
@@ -77,6 +77,71 @@ void register_system();
 #define CONFIG_CONSOLE_MAX_COMMAND_LINE_LENGTH 80
 
 
+static const char *TAG = "FAT";
+// Handle of the wear levelling library instance
+static wl_handle_t s_wl_handle = WL_INVALID_HANDLE;
+// Mount path for the partition
+const char *base_path = "";  // this cannot be "/"
+
+void fat_initialize() {
+    // Setup FAT file system
+    ESP_LOGI(TAG, "Mounting FAT filesystem");
+    const esp_vfs_fat_mount_config_t mount_config = {
+        .max_files = 16,
+        .format_if_mount_failed = true,
+        .allocation_unit_size = CONFIG_WL_SECTOR_SIZE
+    };
+    esp_err_t err = esp_vfs_fat_spiflash_mount_rw_wl(base_path, "storage", &mount_config, &s_wl_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to mount FATFS (%s)", esp_err_to_name(err));
+        return;
+    }
+
+    // Write file
+    ESP_LOGI(TAG, "Opening file");
+    FILE *f = fopen("hello.txt", "w");
+    fprintf(f, "Written using ESP-IDF\n");
+    fclose(f);
+    ESP_LOGI(TAG, "File written");
+
+    // Read file
+    ESP_LOGI(TAG, "Reading file");
+    f = fopen("hello.txt", "r");
+    char line[64];
+    fgets(line, sizeof(line), f);
+    fclose(f);
+
+    // Print data
+    char *pos = strchr(line, '\n');
+    if (pos) {
+        *pos = '\0';
+    }
+    ESP_LOGI(TAG, "Data from file: '%s'", line);
+
+    // List directory
+    ESP_LOGI(TAG, "Listing directory:");
+    DIR *dir = opendir("/");
+    if (dir != NULL) {
+        struct dirent *ent;
+        while ((ent = readdir(dir)) != NULL) {
+            ESP_LOGI(TAG, "  %s", ent->d_name);
+        }
+        closedir(dir);
+    } else {
+        ESP_LOGE(TAG, "Failed to open directory");
+    }
+
+    // Unmount FATFS
+    /*
+    ESP_LOGI(TAG, "Unmounting FAT filesystem");
+    esp_vfs_fat_spiflash_unmount_rw_wl(base_path, s_wl_handle);
+    ESP_LOGI(TAG, "Done");
+    */
+}
+
+int real_main(int argc, char **argv);
+
+
 void app_main() {
     printf("Hello world!\n");
     print_system_info();
@@ -112,4 +177,8 @@ void app_main() {
 
     ESP_ERROR_CHECK(esp_console_start_repl(repl));
     printf("Done registering commands for ESP console.\n");
+
+    fat_initialize();
+
+    // real_main(0, NULL); // mud main
 }
